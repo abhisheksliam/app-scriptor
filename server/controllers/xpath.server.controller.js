5995914e617ae746c6199ce5282778af111edfa7
Abhishek <abhishek.kumar@comprotechnologies.com>
2016-11-16 15:24:23 +0530

    removed support for redis caching

--- server/controllers/xpath.server.controller.js
+++ server/controllers/xpath.server.controller.js
@@ -5,7 +5,6 @@
 
 const router = require('express').Router();
 var Xpath     = require('./../models/app.server.models.xpath');
-var cacheService = require('./../services/cache.server.service');
 
 /**
  *
@@ -69,7 +68,6 @@ exports.addXpath = function (req, res) {
 
                         doc.tags = req.body.tags;
                         doc.xpath.value = req.body.xpath.value;
-                        updateApplicationXpathCache(req.body.app_type, function(){});
 
                         res.json(doc);
                     });
@@ -85,7 +83,6 @@ exports.addXpath = function (req, res) {
                         }
                     });
                 }
-                updateApplicationXpathCache(req.body.app_type, function(){});
                 res.json(xpathData);
             });
         }
@@ -109,11 +106,6 @@ exports.getXpaths = function (req, res) {
 
 exports.getApplicationXpaths = function (req, res) {
 
-    // check value in redis - return if not null else get from database and return
-    var cacheKey = 'xpath-'+req.params.app_type.trim();
-    cacheService.getCacheObject(cacheKey, function(data){
-        if (data) {res.json(data);}
-        else {
             Xpath.find({'app_type': req.params.app_type},function(err, xpathList) {
                 if (err) {
                     res.json({
@@ -123,11 +115,8 @@ exports.getApplicationXpaths = function (req, res) {
                         }
                     });
                 }
-                updateApplicationXpathCache(req.params.app_type, function(){});
                 res.json(xpathList);
             });
-        }
-    });
 };
 
 exports.getApplicationXpathValue = function (req, res) {
@@ -159,18 +148,5 @@ exports.getTaskXpaths = function (req, res) {
             });
         }
         res.json(xpathList);
-    });
-};
-
-function updateApplicationXpathCache (app_type, done) {
-    var cacheKey = 'xpath-'+ app_type.trim();
-
-    Xpath.find({'app_type': app_type},function(err, xpathList) {
-        if (err) {
-            done(err);
-        }
-
-        cacheService.setCacheObject(cacheKey, JSON.stringify(xpathList));
-        done();
     });
 };
