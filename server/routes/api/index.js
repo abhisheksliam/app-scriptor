e0d2c8ea2a27507f1006fd652408b7c38c76a3aa
Abhishek Kumar <abhishek.kumar@comprotechnologies.com>
2017-04-26 18:40:41 +0530

    updated routes

--- server/routes/api/index.js
+++ server/routes/api/index.js
@@ -84,8 +84,7 @@ apirouter.get('/template/:skill_id', templateController.getTemplateBySkillId);
 apirouter.get('/scenario', function(req, res){res.redirect('/api/scenario/sample')});
 apirouter.get('/scenario/:task_id', templateController.getScenarioByTaskId);
 
-apirouter.get('/mapper', function(req, res){res.redirect('/api/mapper/sample')});
-apirouter.get('/mapper/:template_id', templateController.getMapperByTemplatelId);
+apirouter.get('/template/:template_id/mapper', templateController.getMapperByTemplatelId);
 
 apirouter.get('/script', function(req, res){res.redirect('/api/script/sample')});
 apirouter.get('/script/:task_id', templateController.getScriptByTaskId);
