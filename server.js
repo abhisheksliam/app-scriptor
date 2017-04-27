15fd48d0ba609f82259f1683555e08c46d6bbff6
Abhishek Kumar <abhishek.kumar@comprotechnologies.com>
2017-04-27 14:08:20 +0530

    updated for cors

--- server.js
+++ server.js
@@ -47,6 +47,20 @@ app.use(function(req, res, next) {
     return next();
 });
 
+//-----------CORS-------------------
+app.use(function(req, res, next) {
+    var allowedOrigins = ['http://apiui.herokuapp.com', 'https://apiui.herokuapp.com'];
+    var origin = req.headers.origin;
+    if(allowedOrigins.indexOf(origin) > -1){
+        res.setHeader('Access-Control-Allow-Origin', origin);
+    }
+    //res.header('Access-Control-Allow-Origin', 'http://127.0.0.1:8020');
+    res.header('Access-Control-Allow-Methods', 'GET, OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    res.header('Access-Control-Allow-Credentials', true);
+    return next();
+});
+
 // 2. GZIP compression
 app.use(compress());
 
